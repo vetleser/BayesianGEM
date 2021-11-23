468f0d1f400e5c37f642a23da0727be2d83f0486
Jakob Peder Pettersen <jakobpeder.pettersen@gmail.com>
2021-11-23 14:47:38 +0000

    Fixed some typos in the permute parameter script

--- code/gem_smcabc_at_three_conditions_permuted.py
+++ code/gem_smcabc_at_three_conditions_permuted.py
@@ -37,7 +37,7 @@ Yobs = {'rae':Yobs_batch['data'],
 
 n_permutations = 3
 random_seed = 158
-rng = np.random.Generator(random_seed)
+rng = np.random.default_rng(random_seed)
 path = os.path.dirname(os.path.realpath(__file__)).replace('code','')
 params = pd.read_csv(os.path.join(path,'data/model_enzyme_params.csv'),index_col=0)
 permuted_params = [permute_parameters.permute_params(params, rng) for _ in range(n_permutations)]
@@ -48,12 +48,14 @@ permuted_params = [permute_parameters.permute_params(params, rng) for _ in range
 # In[ ]:
 
 
-priors = dict()
-for ind in params.index: 
-    for col in ['Tm','Topt','dCpt']: 
-        priors['{0}_{1}'.format(ind,col)] = abc.RV('normal',
-                                                      loc=permuted_params.loc[ind,col],
-                                                      scale=permuted_params.loc[ind,col+'_std'])
+priors = [dict() for _ in range(n_permutations)]
+
+for i in range(n_permutations):
+    for ind in params.index: 
+        for col in ['Tm','Topt','dCpt']: 
+            priors[i]['{0}_{1}'.format(ind,col)] = abc.RV('normal',
+                                                          loc=permuted_params[i].loc[ind,col],
+                                                          scale=permuted_params[i].loc[ind,col+'_std'])
 
 
 # #### Define model settings
@@ -63,17 +65,17 @@ for ind in params.index:
 
 min_epsilon = -1 # equivalent to r2 score of 1
 population_size = 100
-outfiles = [f'../results/smcabc_gem_three_conditions_permuted_{i}_save_all_particles.pkl' for i in range(n_permutations)]
+outfiles = [f'../data/smcabc_gem_three_conditions_permuted_{i}_save_all_particles.pkl' for i in range(n_permutations)]
 
 
 # In[ ]:
 
 models = []
-for outfile in outfiles:
+for i, outfile in enumerate(outfiles):
     if not os.path.exists(outfile):
         print('Initialize model')
         model = abc.SMCABC(GEMS.simulate_at_three_conditions_2,
-                            priors,
+                            priors[i],
                             min_epsilon,
                             population_size,
                             GEMS.distance_2,
@@ -93,3 +95,4 @@ for i, model in enumerate(models):
     model.run_simulation()
 
 print(f'{datetime.datetime.now()} => DONE')
+
