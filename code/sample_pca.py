d4c0e3c287dfedc67cde913fa16e548505a1861d
Jakob Peder Pettersen <jakobpeder.pettersen@gmail.com>
2022-04-21 15:49:56 +0200

    Changed downstream PCA generation code

--- code/sample_pca.py
+++ code/sample_pca.py
@@ -4,7 +4,9 @@
 import pickle
 import pandas as pd
 import numpy as np
+import evo_etc
 from sklearn.decomposition import PCA
+import multiprocessing
 
 # Convenient pickle wrappers
 def load_pickle(filename):
@@ -13,6 +15,33 @@ def load_pickle(filename):
 def dump_pickle(obj,filename):
     return pickle.dump(obj=obj,file=open(file=filename, mode='wb'))
 
+def build_a_dataframe_for_all_particles(results, n_priors = 128, r2_threshold = 0.9, convert_results=False):
+    columns = list(results.all_particles[0].keys())
+    columns.sort()
+    print("Iterating over particles")
+    data = list()
+    for p in results.all_particles:
+        if convert_results:
+            p_refined = evo_etc.convert_to_raw_particle(p)
+        else:
+            p_refined = p
+        data.append([p_refined[k] for k in columns])
+    print("Creating Data Frame")
+    df = pd.DataFrame(data=data,columns=columns)
+    df['r2'] = results.all_distances
+    print(df.shape)
+    
+    # Remove samples with a R2 score smaller than -3
+    print("Doing filtering and labelling of Data Frame")
+    df['r2'] = -df['r2']
+    sel_index = df.index[df['r2']>-3]    
+    df = df.loc[sel_index,:]
+    df["period"] = "Intermediate"
+    df.loc[:n_priors,"period"] = "Prior"
+    df.loc[df["r2"] > r2_threshold,"period"] = 'Posterior'
+    print(df.shape)
+
+    return df
 
 def combine_dataframes_for_models(df_dict):
     # augmented_df_list =[ df.assign(model = lambda df: label)  for df, label in zip(df_list, index)]
@@ -36,6 +65,11 @@ def perform_pca_on_parameters(df):
     return PCS, pca.explained_variance_ratio_
 
 model_frame = load_pickle("../results/permuted_smcabc_res/result_model_frame.pkl")
+with multiprocessing.Pool(10) as p:
+    particle_df_map = p.map(build_a_dataframe_for_all_particles,model_frame.model)
+    model_frame["particle_df"] = list(particle_df_map)
+
+dump_pickle(model_frame["particle_df"], "../results/permuted_smcabc_res/particle_df.pkl")
 
 full_particle_df_dict = {distribution: df for distribution, df in model_frame["particle_df"].iteritems()}
 combined_df = combine_dataframes_for_models(full_particle_df_dict)
