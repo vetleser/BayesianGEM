5ed6d9ef2b058cc1b078aeea81aebfff996e6eb2
Jakob Peder Pettersen <jakobpeder.pettersen@gmail.com>
2022-04-22 15:51:14 +0200

    Added job script to evolutionary algorithm

---
+++ code/gem_smcevo_at_three_conditions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -J gem_smcevo_at_three_conditions               # sensible name for the job
+#SBATCH --nodes=1                    
+#SBATCH -c 40
+#SBATCH -t 100:00:00             # Upper time limit for the job
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=end
+#SBATCH --mail-user=jakob.p.pettersen@ntnu.no
+#SBATCH -p CPUQ
+#SBATCH --account=nv-ibt
+
+WORKDIR=${SLURM_SUBMIT_DIR}
+cd ${WORKDIR}
+module purge
+module load Anaconda3/2020.07
+source /cluster/apps/eb/software/Anaconda3/2020.07/etc/profile.d/conda.sh
+conda activate etcFBA
+python gem_smcevo_at_three_conditions.py &> "../results/gem_smcevo_at_three_conditions.log"
