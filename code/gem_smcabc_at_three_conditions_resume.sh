a69e1973cf205c9812802aec52f429a9df066f5c
Jakob Peder Pettersen <jakobpeder.pettersen@gmail.com>
2022-04-28 11:44:58 +0200

    Removed job array specifyer for resume job

--- code/gem_smcabc_at_three_conditions_resume.sh
+++ code/gem_smcabc_at_three_conditions_resume.sh
@@ -4,7 +4,6 @@
 #SBATCH --nodes=1                    
 #SBATCH -c 20
 #SBATCH -t 100:00:00             # Upper time limit for the job
-#SBATCH --array=0-7
 #SBATCH --mail-type=begin
 #SBATCH --mail-type=end
 #SBATCH --mail-user=jakob.p.pettersen@ntnu.no
