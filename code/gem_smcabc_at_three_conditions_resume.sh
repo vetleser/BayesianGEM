e865de32ef92bce5a730a8b5464cd7008b03ad70
Jakob Peder Pettersen <jakobpeder.pettersen@gmail.com>
2022-04-28 11:32:14 +0200

    Resuming computations which crashed seemingly by random chance

---
+++ code/gem_smcabc_at_three_conditions_resume.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name="gem_smcabc_at_three_conditions_resume"               # sensible name for the job
+#SBATCH --output=gem_smcabc_at_three_conditions_resume.out
+#SBATCH --nodes=1                    
+#SBATCH -c 20
+#SBATCH -t 100:00:00             # Upper time limit for the job
+#SBATCH --array=0-7
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=end
+#SBATCH --mail-user=jakob.p.pettersen@ntnu.no
+#SBATCH -p CPUQ
+#SBATCH --account=share-nv-ibt
+#SBATCH --export=NONE
+
+WORKDIR=${SLURM_SUBMIT_DIR}
+cd ${WORKDIR}
+module purge
+module load Anaconda3/2020.07
+source ~/.bash_profile
+conda activate etcFBA
+python gem_smcabc_at_three_conditions_resume.py &> "../results/permuted_smcabc_res/gem_smcabc_at_three_conditions_resume.log"
