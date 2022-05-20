9befba1e92635ed31374a274ae5c7c25cc89a746
Jakob Peder Pettersen <jakobpeder.pettersen@gmail.com>
2022-05-20 13:44:55 +0200

    Added evolutionary FVA

---
+++ code/evo_fva.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -J evo_fva               # sensible name for the job
+#SBATCH --output=evo_fva.out
+#SBATCH --nodes=1                    
+#SBATCH -c 20
+#SBATCH -t 20:00:00             # Upper time limit for the job
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=end
+#SBATCH --mail-user=jakob.p.pettersen@ntnu.no
+#SBATCH -p CPUQ
+#SBATCH --account=share-nv-ibt
+#SBATCH --export=NONE
+
+WORKDIR=${SLURM_SUBMIT_DIR}
+cd ${WORKDIR}
+module purge
+module load Anaconda3/2020.07
+source ~/.bash_profile
+conda activate etcFBA
+python evo_fva.py &> "../results/evo_fva.log"
