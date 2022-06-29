74760b85a796cd7c9e7f214771dfc2cbe380cc6c
Jakob Peder Pettersen <jakobpeder.pettersen@gmail.com>
2022-06-29 11:31:52 +0200

    Made a small change in switching back to original branch

--- code/benchmark_performance.sh
+++ code/benchmark_performance.sh
@@ -9,4 +9,4 @@ python benchmark_performance.py
 git checkout cfe99d7142e93ca12a36d8bb64d0a746d21c230f # Takes us back to the time the etcFBA framework used cobrapy
 echo "Benchmarking with cobrapy"
 python benchmark_performance.py
-git checkout HEAD
+git switch -
