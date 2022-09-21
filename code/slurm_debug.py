5a415827be9c80626fc8cd7ae73e9e16f7cb0ffa
Jakob Peder Pettersen <jakobpeder.pettersen@gmail.com>
2022-09-21 13:14:11 +0200

    Modified debug script to same in the same folder

--- code/slurm_debug.py
+++ code/slurm_debug.py
@@ -6,6 +6,4 @@ import os
 
 task_idx = int(os.environ["SLURM_ARRAY_TASK_ID"])
 
-pickle.dump(task_idx,file=open(f"../results/debug_results_{task_idx}.pkl",'wb'))
-
-
+pickle.dump(task_idx,file=open(f"debug_results_{task_idx}.pkl",'wb'))
