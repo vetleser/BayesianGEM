fd2405c7f4af6b4542dfe61bf30a7d5678a178b5
Jakob Peder Pettersen <jakobpeder.pettersen@gmail.com>
2022-09-21 13:21:17 +0200

    Some minor changes in the debug setup

--- code/slurm_debug.py
+++ code/slurm_debug.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import pickle
+import dill
 import os
+
 
 task_idx = int(os.environ["SLURM_ARRAY_TASK_ID"])
 
-pickle.dump(task_idx,file=open(f"debug_results_{task_idx}.pkl",'wb'))
+dill.dump(task_idx,file=open(f"../results/debug_results_{task_idx}.pkl",'wb'))
