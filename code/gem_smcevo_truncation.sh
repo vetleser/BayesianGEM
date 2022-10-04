789acbaee87d80b82c92a3f7e5cb9da1ee81e202
Jakob Peder Pettersen <jakobpeder.pettersen@gmail.com>
2022-10-04 13:58:07 +0200

    Changed memory requirements for computational scripts

--- code/gem_smcevo_truncation.sh
+++ code/gem_smcevo_truncation.sh
@@ -5,7 +5,7 @@
 #SBATCH -c 20
 #SBATCH -t 50:00:00             # Upper time limit for the job
 #SBATCH --array=0-7
-#SBATCH --mem=50G
+#SBATCH --mem=100G
 #SBATCH --mail-type=begin
 #SBATCH --mail-type=end
 #SBATCH --mail-user=jakob.p.pettersen@ntnu.no
