d9315397fd5a76e8a3b10ec4fb3968d80aae47f3
Vetle Skaar Eriksen <vetleser@stud.ntnu.no>
2025-01-07 15:19:25 +0100

    Changed python location based on new environments

--- code/evo_fva.sh
+++ code/evo_fva.sh
@@ -5,17 +5,8 @@
 #SBATCH -c 1
 #SBATCH -t 10:00:00             # Upper time limit for the job
 #SBATCH --mem=120G
-#SBATCH --mail-type=begin
-#SBATCH --mail-type=end
-#SBATCH --mail-user=jakob.p.pettersen@ntnu.no
-#SBATCH -p CPUQ
-#SBATCH --account=share-nv-ibt
 #SBATCH --export=NONE
 
 WORKDIR=${SLURM_SUBMIT_DIR}
 cd ${WORKDIR}
-module purge
-module load Anaconda3/2020.07
-source ~/.bash_profile
-conda activate etcFBA
-python evo_fva.py &> "../results/crowdingDE/evo_fva.log"
+/triumvirate/home/vetleser/.conda/envs/etcFBA_v2/bin/python3 evo_fva.py &> "../results/crowdingDE/evo_fva.log"
--- code/evo_pca.sh
+++ code/evo_pca.sh
@@ -5,17 +5,8 @@
 #SBATCH -c 1
 #SBATCH --mem=10G
 #SBATCH -t 01:00:00             # Upper time limit for the job
-#SBATCH --mail-type=begin
-#SBATCH --mail-type=end
-#SBATCH --mail-user=jakob.p.pettersen@ntnu.no
-#SBATCH -p CPUQ
-#SBATCH --account=share-nv-ibt
 #SBATCH --export=NONE
 
 WORKDIR=${SLURM_SUBMIT_DIR}
 cd ${WORKDIR}
-module purge
-module load Anaconda3/2020.07
-source ~/.bash_profile
-conda activate etcFBA
-python evo_pca.py &> "../results/crowdingDE/evo_pca.log"
+/triumvirate/home/vetleser/.conda/envs/etcFBA_v2/bin/python3 evo_pca.py &> "../results/crowdingDE/evo_pca.log"
